9d00fb3de8daf4dbde8240d9fbab6b1d1d352b88
Sahil Rzayev <sahilrzayev200d@gmail.com>
2026-08-12 22:46:09 +0400

    feat: implement application startup logic with single-instance mutex enforcement and dependency injection container configuration

--- DocumentSearch/App.xaml.cs
+++ DocumentSearch/App.xaml.cs
@@ -1,17 +1,44 @@
 using DocumentSearch.Services;
 using DocumentSearch.ViewModels;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Windows;
-using System.Threading.Tasks;
 
 namespace DocumentSearch;
 
 public partial class App : Application
 {
+    private static Mutex? _mutex;
     private ServiceProvider? _serviceProvider;
+
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+    [DllImport("user32.dll")]
+    private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool IsIconic(IntPtr hWnd);
+
+    private const int SW_RESTORE = 9;
 
     protected override void OnStartup(StartupEventArgs e)
     {
+        const string mutexName = "Global\\E-Student_SingleInstance_Mutex_D1A39F71-B88E-47C2-823E-91C083758A11";
+        _mutex = new Mutex(true, mutexName, out bool createdNew);
+
+        if (!createdNew)
+        {
+            BringExistingInstanceToForeground();
+            Shutdown();
+            return;
+        }
+
         base.OnStartup(e);
 
         DispatcherUnhandledException += (sender, args) =>
@@ -30,6 +57,36 @@ public partial class App : Application
         mainWindow.Show();
         
         // Not: Güncelleme kontrolü NavigationViewModel constructor'ında yapılıyor
+    }
+
+    private static void BringExistingInstanceToForeground()
+    {
+        try
+        {
+            var currentProcess = Process.GetCurrentProcess();
+            var processes = Process.GetProcessesByName(currentProcess.ProcessName);
+
+            foreach (var process in processes)
+            {
+                if (process.Id != currentProcess.Id)
+                {
+                    IntPtr hWnd = process.MainWindowHandle;
+                    if (hWnd != IntPtr.Zero)
+                    {
+                        if (IsIconic(hWnd))
+                        {
+                            ShowWindow(hWnd, SW_RESTORE);
+                        }
+                        SetForegroundWindow(hWnd);
+                    }
+                    break;
+                }
+            }
+        }
+        catch
+        {
+            // Ignore errors when searching for processes
+        }
     }
 
     private void ConfigureServices(IServiceCollection services)
@@ -58,6 +115,16 @@ public partial class App : Application
 
     protected override void OnExit(ExitEventArgs e)
     {
+        if (_mutex != null)
+        {
+            try
+            {
+                _mutex.ReleaseMutex();
+            }
+            catch { }
+            _mutex.Dispose();
+        }
+
         _serviceProvider?.Dispose();
         base.OnExit(e);
     }
--- installer.iss
+++ installer.iss
@@ -4,7 +4,7 @@
 #define MyAppExeName "E-Student.exe"
 
 #ifndef MyAppVersion
-#define MyAppVersion "2.3.8"
+#define MyAppVersion "2.3.9"
 #endif
 
 [Setup]
